629aaabf49b03d2322479bd90ac7929208195774
nabi fan <nabi.fan@onelab.tw>
2021-12-29 19:03:03 +0800

    feat: add networkspeed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NetworkSpeed from './NetworkSpeed';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+  
+    <NetworkSpeed />
   </React.StrictMode>,
   document.getElementById('root')
 );
