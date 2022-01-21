0aac340b5db76d6b8a01877677247106b80c8055
nabi fan <nabi.fan@onelab.tw>
2022-01-21 19:01:25 +0800

    feat: add eslint

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const shadow = {
   boxShadow: '0 0 10px 10px #eaeaea',
   padding: 20, // 省略 px，樣式會自動帶入單位變成 '20px'
 };
-const Counter = () => {
+function Counter() {
     const [count, setCount] = useState(5);
     const handleChevron = (type) => () => {
         if (type === 'up') {
             setCount(count + 1)
-        } else if(type === "down") {
+        } else if(type === 'down') {
             setCount(count - 1);
         }
     }
@@ -24,17 +24,24 @@ const Counter = () => {
     return (
         <div className="container" style={shadow}>
             {count < limtUP && 
-                <div className="chevron chevron-up" onClick={handleChevron('up')}></div>
+              <div className="chevron chevron-up" onKeyPress={handleChevron('up')} role="presentation" />
             }
             <div className="number">{count}</div>
             {count > limtDown &&
-                <div className="chevron chevron-down" onClick={handleChevron('down')}></div>
+                <div className="chevron chevron-down" onKeyPress={handleChevron('down')} role="presentation" />
             }
+            <div
+              onClick={handleChevron}
+              onKeyPress={handleChevron}
+              role="button"
+              tabIndex="0">
+              Save
+            </div>
         </div>
     )
 }
 
-const App = () => {
+function App() {
     return (
         <Counter />
     )
